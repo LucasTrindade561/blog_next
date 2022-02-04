94c2b069f67035e7084857b07a56fc8e3958b7e0
Lucas Trindade <nh.lucass@gmail.com>
2022-02-04 18:55:20 -0300

    home test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,34 +6,21 @@ import Document, {
   NextScript,
   Head,
 } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        });
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => App,
+        enhanceComponent: (Component) => Component,
+      });
 
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
   }
+
   render() {
     return (
       <Html lang="pt-BR">
@@ -46,3 +33,5 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+export default MyDocument;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,34 @@
-export default function Home() {
-  return <h1>Hello World!</h1>;
+import { GetStaticProps } from 'next';
+import { PostData } from '../domain/posts/post';
+
+const getPosts = async (): Promise<PostData[]> => {
+  const posts = await fetch(
+    'https://evening-stream-83444.herokuapp.com/api/posts?populate=*',
+  );
+  const jsonPosts = await posts.json();
+  const jsonWithoutData = jsonPosts.data;
+  return jsonWithoutData;
+};
+
+export type HomeProps = {
+  posts: PostData[];
+};
+
+export default function Home({ posts }: HomeProps) {
+  return (
+    <div>
+      {posts.map((post) => (
+        <h2 key={post.attributes.slug}>{post.attributes.title}</h2>
+      ))}
+    </div>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getPosts();
+
+  return {
+    props: { posts },
+    // revalidate: 5,
+  };
+};
